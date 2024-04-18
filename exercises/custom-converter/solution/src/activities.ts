d6d270638093a9f346cf35e5436804a6b1ff8ce7
angelazhou32 <angela.zhou@temporal.io>
2024-04-18 18:29:52 -0400

    change nvmrc to 20 and get rid of context for logging in activities

--- exercises/custom-converter/solution/src/activities.ts
+++ exercises/custom-converter/solution/src/activities.ts
@@ -2,8 +2,7 @@ import * as activity from '@temporalio/activity';
 import { ApplicationFailure } from '@temporalio/activity';
 
 export async function converterActivity(input: string): Promise<string> {
-  const context = activity.Context.current();
-  context.log.info('User is successfully logged', { input });
+  activity.log.info('User is successfully logged', { input });
   // throw ApplicationFailure.create({ nonRetryable: true, message: `Activity failed:` })
   return `Received ${input}`;
 }
